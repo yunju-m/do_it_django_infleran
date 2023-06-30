28d94fb59277112dd6c40f57f7ac4c7dd021b436
yunju-m <dbswnfnal@naver.com>
2023-06-30 18:29:36 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # 리스트를 출력하는 django 기본 라이브러리
 # index함수 대신 사용
 class PostList(ListView):
     model = Post
-    template_name = 'blog/index.html'   #기본 post_list.html 이름으로 사용 --> 경로지정 가능
     ordering = '-pk'                    # 기본키 역순으로 출력
+    # template_name = 'blog/index.html'   #기본 post_list.html 이름으로 사용 --> 경로지정 가능
+
+# 하나의 리스트를 불러오는 django 기본 라이브러리
+# single_post_page함수 대신 사용
+class PostDetail(DetailView):
+    model = Post
 
 # def index(request):
 #     # 기본키 역순으로 출력하기
@@ -22,13 +27,13 @@ class PostList(ListView):
 #     )
 
 # 각 고유 블로그 페이지 내용 불러오기
-def single_post_page(request, pk):
-    posts = Post.objects.get(pk=pk)
+# def single_post_page(request, pk):
+#     posts = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'blog/single_page.html',
-        {
-            'post': posts,
-        }
-    )
+#     return render(
+#         request,
+#         'blog/single_page.html',
+#         {
+#             'post': posts,
+#         }
+#     )
